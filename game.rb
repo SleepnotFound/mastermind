86b94195a35786a401eeccecc9c365fac93ae132
Javi <jav.olivares3@gmail.com>
2022-02-14 16:02:26 -0600

    results method returns winner

--- game.rb
+++ game.rb
@@ -1,15 +1,16 @@
 class Game
-  attr_reader :password, :attempt
+  attr_reader :password, :attempt, :mode
 
   def initialize
     @password = nil
     @attempt = nil
+    @mode = nil
     mode_selection
   end
   def mode_selection
-    mode = gets.chomp
-    if mode == "1" then play_maker 
-    elsif mode == "2" then play_breaker 
+    @mode = gets.chomp
+    if self.mode == "1" then play_maker 
+    elsif self.mode == "2" then play_breaker 
     else 
       puts "Not a valid choice. Enter 1 for 'maker' or 2 for 'breaker'"
       mode_selection 
@@ -19,11 +20,17 @@ end
 
 def play_breaker
   puts "playing as breaker"
+  round = 12
   @password = Computer.new.gen_code
   puts "computer's code: #{color_pegs(password.clone)}"
-  @attempt = User.new.user_turn
-  find_clues(self.password.clone, self.attempt.clone)
-  puts "originals: #{password} vs #{attempt}"
+  until attempt == password || round == 13
+    puts "round:#{round}"
+    @attempt = User.new.user_turn
+    find_clues(self.password.clone, self.attempt.clone)
+    round += 1
+  end
+  results
+  #puts "originals: #{password} vs #{attempt}"
 end
 
 def play_maker
@@ -50,5 +57,13 @@ def find_clues(pass, guess)
       puts "#{color_pegs("white")}"
       pass.delete_at(pass.index(g))
     end
+  end
+end
+
+def results
+  if attempt == password
+    puts mode == "1" ? "you lost" : "you won"
+  else
+    puts mode == "1" ? "you won" : "you lost"
   end
 end
